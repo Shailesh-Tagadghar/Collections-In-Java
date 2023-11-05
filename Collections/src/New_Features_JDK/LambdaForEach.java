b363a2b7a2242263eec830258688ce5b0b66e5a0
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-11-05 18:39:12 +0530

    For Each Method using Lambda Exp.

---
+++ Collections/src/New_Features_JDK/LambdaForEach.java
@@ -0,0 +1,27 @@
+package New_Features_JDK;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class LambdaForEach {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		
+		list.add(25);
+		list.add(34);
+		list.add(45);
+		list.add(85);
+		
+//		Iterator<Integer> itr = list.iterator();
+//		while(itr.hasNext()) {
+//			System.out.println(itr.next());
+//		}
+		
+//		Consumer c = obj -> System.out.println(obj);
+//		list.forEach(c);
+		
+		list.forEach(obj -> System.out.println(obj));
+	}
+}
