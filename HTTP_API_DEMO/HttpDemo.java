fd6ac1a1036fbb1ce1baf0aff043263a4b22ace1
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-11-26 17:52:14 +0530

    Update HttpDemo.java

--- HTTP_API_DEMO/HttpDemo.java
+++ HTTP_API_DEMO/HttpDemo.java
@@ -1,5 +1,40 @@
 package HTTP_API_DEMO;
 
-public class HttpDemo {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class MyHttpDemo {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		 /*
+		  1. HTTP Client :
+		     Will act as client (browser)
+		     Will sends Request to Server/API and brings the Response
+		  */
+		
+		HttpClient client = HttpClient.newBuilder().build();
+		
+		
+		/*
+		 2. HTTP Request : 
+		    
+		 */
+		
+		HttpRequest req = HttpRequest.newBuilder()
+				.GET()
+				.uri(URI.create("https://dog.ceo/api/breeds/list/all"))
+				.build();
+		
+		/*
+		 3. HTTP Response : 
+		    
+		 */
+		
+		HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString() );
+		System.out.println("Status : "+response.statusCode());
+		System.out.println("Body : "+response.body());
+		
+	}
 }
